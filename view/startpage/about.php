0d3bb860def434c5676d7399f5d422d08eadc09d
Robin Blixter <r.blixter89@gmail.com>
2020-01-08 21:45:17 +0100

    Updated the about-page

--- view/startpage/about.php
+++ view/startpage/about.php
@@ -3,4 +3,13 @@
 namespace Anax\View;
 
 ?><h1>About</h1>
-<p>This site is created by Robin Blixter for the course Ramverk1 at Blekinge Tekniska Högskola.</p>
+
+<h4>StackUnderflow</h4>
+<p>This is not a real site, it's a Stackoverflow-copy made for the course Ramverk1 at Blekinge Tekniska Högskola.</p>
+</p>On this site the user can ask questions, which can be answered by others users. All questions and answers can be commented and voted. The user can mark an answer as an accepted answer.</p>
+<p>Here's the repo for this website on GitHub: <a href="https://github.com/Blixter/ramverk1-proj">https://github.com/Blixter/ramverk1-proj</a>.
+
+<h4>Me</h4>
+<p>Robin Blixter is the creator of the site. He's 30 years old and live in Gothenburg, Sweden. Ha has two kids at home and is currently studying at Blekinge Tekniska Högskola.
+Robin is interested in technologies, fishing and hockey.</p>
+<p>Here's the repo for the course Ramverk1:  <a href="https://github.com/Blixter/ramverk1">https://github.com/Blixter/ramverk1</a>.
